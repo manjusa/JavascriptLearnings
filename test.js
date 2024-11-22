e2934031904e72a4b54366abd0ea27fcccdb1ceb
manjusa <thizismanju@mail.com>
2024-11-22 18:50:06 +1100

    Setting up pipeline operator and  falsy and truthy values

---
+++ test.js
@@ -0,0 +1,23 @@
+// FALSY - All below prints false
+//const val = 0;
+//const val = false;
+//const val = -0;
+//const val = "";
+//const val = ``;
+//const val = null;
+//const val = undefined;
+//const val = NaN;
+
+//TRUTHY
+//const val = true;
+//const val = "0";
+//const val = `''`;
+//const val = [];
+//const val = {};
+//const val = () => {};
+
+if (val) {
+  console.log("truthy");
+} else {
+  console.log("false");
+}
