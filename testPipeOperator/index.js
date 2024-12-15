046c8dd29ce1ab95a94e2395541790ecf5802625
manjusa <thizismanju@mail.com>
2024-12-15 15:25:33 +1100

    truth vs falsy

--- testPipeOperator/index.js
+++
@@ -1,8 +0,0 @@
-const addQuestion = (str) => str + "?";
-const toUpperCase = (str) => str.toUpperCase();
-const customString = (str) => str.concat("How are you");
-const reverseString = (str) => str.split("").reverse().join("");
-const result = "Hey Manju " |> toUpperCase |> customString |> addQuestion;
-const reverseResult = result |> reverseString;
-console.log(result); // Output: "HEY MANJU How are you?"
-console.log(reverseResult); // Output: "?uoy era woH UJNAM YEH"
